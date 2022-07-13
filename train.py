76fb44e045683846ed6f2d686fbc5d2886154b18
Larry234 <lazybear17@gmail.com>
2022-07-13 17:24:08 +0800

    baseline training

--- train.py
+++ train.py
@@ -1,5 +1,16 @@
 import torch
 from resent import resnet34
+import argparse
 
 from torchvision.datasets import CIFAR100
 import torchvision.transforms as transforms
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--lr', default=0.1, type=float)
+    parser.add_argument('--epoch', default=50, type=int)
+    parser.add_argument('--log-dir', help='tensorboard directory')
+    
+    args = parser.parse_args()
+    
